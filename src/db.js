3821cabbf85c420a3138d6cb280814143cf063b4
Nicolás Rodriguez <117177426+NiKolas1828@users.noreply.github.com>
2025-03-26 15:48:13 -0500

    conexion db

--- src/db.js
+++ src/db.js
@@ -2,12 +2,26 @@ import pg from 'pg';
 
 // Configuración de la conexión
 export const pool = new pg.Pool({
-  user: 'postgres.rkpwagjxvdajggqvdoyk', // Usuario de la base de datos
-  host: 'aws-0-us-west-1.pooler.supabase.com',
-  database: 'postgres', // Nombre de la base de datos
-  password: 'mmFC5EnnZcpQTB0B', // Contraseña de la base de datos
-  port: 6543, // Puerto de PostgreSQL
+  user: 'postgres', // Usuario de la base de datos
+  host: 'database-1.cbaw20om6wxc.us-east-2.rds.amazonaws.com',
+  database: 'Codekids', // Nombre de la base de datos
+  password: 'posgrescodekids', // Contraseña de la base de datos
+  port: 5432, // Puerto de PostgreSQL
   ssl: {
     rejectUnauthorized: false, // Necesario para conexiones SSL
   },
 });
+
+async function verificarConexion() {
+  try {
+    const client = await pool.connect();
+    const res = await client.query('SELECT NOW() AS fecha_hora'); // Consulta simple
+    console.log('Conexión exitosa:', res.rows[0].fecha_hora);
+    client.release(); // Liberar conexión
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
+  }
+}
+
+// Llamar a la función para probar la conexión
+verificarConexion();
